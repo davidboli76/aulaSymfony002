eb90549da0ab34fd5107d72082b9fc9b95b6762d
David Barreto <davidbarretobk@gmail.com>
2025-06-15 18:13:54 -0300

    rotas e nova página

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,51 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+
+        $categories = [
+            ['title' => 'Mundo',            'text' => 'Notícias sobre o Mundo'],
+            ['title' => 'Brasil',           'text' => 'Notícias sobre o Brasil'],
+            ['title' => 'Tecnologia',       'text' => 'Notícias sobre Tecnologia'],
+            ['title' => 'Design',           'text' => 'Notícias sobre Design'],
+            ['title' => 'Cultura',          'text' => 'Notícias sobre Cultura'],
+            ['title' => 'Negócios',         'text' => 'Notícias sobre Negócios'],
+            ['title' => 'Política',         'text' => 'Notícias sobre Política'],
+            ['title' => 'Opinião',          'text' => 'Notícias sobre Opinião'],
+            ['title' => 'Ciência',          'text' => 'Notícias sobre Ciência'],
+            ['title' => 'Saúde',            'text' => 'Notícias sobre Saúde'],
+            ['title' => 'Estilo',           'text' => 'Notícias sobre Estilo'],
+            ['title' => 'Viagens',          'text' => 'Notícias sobre Viagens']
+        ];
+        
+        $pageTitle = "Sistema de notícias";
         return $this->render('home/home.html.twig', [
-            
+            'categories' => $categories,
+            'pageTitle' => $pageTitle,
+        ]);
+    }
+
+    #[Route('/categoria/{slug}', name: 'app_category')]
+    public function category(string $slug=null): Response
+    {
+        $categories = [
+            ['title' => 'Mundo',            'text' => 'Notícias sobre o Mundo'],
+            ['title' => 'Brasil',           'text' => 'Notícias sobre o Brasil'],
+            ['title' => 'Tecnologia',       'text' => 'Notícias sobre Tecnologia'],
+            ['title' => 'Design',           'text' => 'Notícias sobre Design'],
+            ['title' => 'Cultura',          'text' => 'Notícias sobre Cultura'],
+            ['title' => 'Negócios',         'text' => 'Notícias sobre Negócios'],
+            ['title' => 'Política',         'text' => 'Notícias sobre Política'],
+            ['title' => 'Opinião',          'text' => 'Notícias sobre Opinião'],
+            ['title' => 'Ciência',          'text' => 'Notícias sobre Ciência'],
+            ['title' => 'Saúde',            'text' => 'Notícias sobre Saúde'],
+            ['title' => 'Estilo',           'text' => 'Notícias sobre Estilo'],
+            ['title' => 'Viagens',          'text' => 'Notícias sobre Viagens']
+        ];
+
+        $pageTitle = "Notícias sobre" . $slug;
+        return $this->render('home/category.html.twig', [
+            'categories' => $categories,
+            'pageTitle' => $pageTitle,
         ]);
     }
 }
