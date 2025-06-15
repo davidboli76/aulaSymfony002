6596caaaf4119883132b686e7643b50ca4b21bd6
David Barreto <davidbarretobk@gmail.com>
2025-06-15 16:15:42 -0300

    debug

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/home.html.twig', [
